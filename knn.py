67fa9600028d40fa1470c8733b749c19c1361a52
adammcpherson <aomcpherson@gmail.com>
2018-01-19 16:09:13 -0700

    Add knn.py file that implements my algorithm

---
+++ knn.py
@@ -0,0 +1,45 @@
+import numpy as np
+
+class KNeighborsClassifier():
+
+    def __init__(self, k=3):
+        self.k = k
+
+    def fit(self, data, targets):
+        return KNeighborsModel(self.k, data, targets)
+
+class KNeighborsModel(object):
+
+    def __init__(self, k, data, targets):
+        self.k = k
+        self.data = data
+        self.targets = targets
+
+    def predict(self, data_test):
+        return self.knn(self.k, self.data, self.targets, data_test)
+
+    def knn(self, k, data, targets, data_test):
+        #training data
+        #training target
+        #k 
+        nInputs = np.shape(data_test)[0]
+        closest = np.zeros(nInputs)
+
+        for n in range(nInputs):
+            #compute distances
+            distances = np.sum((data - data_test[n,:])**2, axis=1)
+
+            #find nearest neighbor
+            indices = np.argsort(distances,axis = 0)
+
+            classes = np.unique(targets[indices[:k]])
+            if len(classes)==1:
+                closest[n] = np.unique(classes)
+            else:
+                counts = np.zeros(max(classes)+1)
+                for i in range(k):
+                    counts[targets[indices[i]]] += 1
+                    closest[n] = np.max(counts)
+
+        return closest
+
